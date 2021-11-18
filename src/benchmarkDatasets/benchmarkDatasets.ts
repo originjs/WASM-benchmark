a4c4b5ca305035f1ed009c9dac6315bd2b6fa3f1
chenshuming <csmdev@hotmail.com>
2021-11-18 10:52:11 +0800

    refactor: benchmarkDatasets remove signle quote

--- src/benchmarkDatasets/benchmarkDatasets.ts
+++ src/benchmarkDatasets/benchmarkDatasets.ts
@@ -6,17 +6,17 @@ import collisionDetectionModule from '~/browser_benchmark/collisionDetection'
 import collisionDetectionWasmTest from '@/benchmarkTests/collisionDetectionWasmTest'
 
 const benchmarkDatasets = {
-    'sumInt': {
-        'Module': sumIntModule,
-        'testbench': sumIntWasmTest,
-        'url': '/browser_benchmark/sumInt.wasm',
-        'dataSize': 0x8000000
+    sumInt: {
+        Module: sumIntModule,
+        testbench: sumIntWasmTest,
+        url: '/browser_benchmark/sumInt.wasm',
+        dataSize: 0x8000000
     },
-    'collisionDetection': {
-        'Module': collisionDetectionModule,
-        'testbench': collisionDetectionWasmTest,
-        'url': '/browser_benchmark/collisionDetection.wasm',
-        'dataSize': 0x4000
+    collisionDetection: {
+        Module: collisionDetectionModule,
+        testbench: collisionDetectionWasmTest,
+        url: '/browser_benchmark/collisionDetection.wasm',
+        dataSize: 0x4000
     }
 }
 
