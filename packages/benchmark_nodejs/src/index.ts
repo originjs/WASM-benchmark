1f9a982cb4958a331eb6d66c04ce9a1a9868df86
chenshuming <csmdev@hotmail.com>
2021-12-03 14:52:38 +0800

    fix(benchmark_nodejs): remove ts-ignore in index.ts

--- packages/benchmark_nodejs/src/index.ts
+++ packages/benchmark_nodejs/src/index.ts
@@ -2,18 +2,22 @@ import benchmarkDatasets from './benchmarkDatasets/benchmarkDatasets';
 import { runBenchmark } from './runBenchmark';
 import { question } from 'readline-sync';
 
+type TestNames = {
+  [index: number]: string;
+};
+
 (async () => {
   let result = { testResults: [] };
   const jsonPath = './benchmark_nodejs_result.json';
   const startIndex = 2;
 
   console.log('\n=== javascript webassembly benchmark in nodejs ===\n');
-  let [index, map] = [startIndex, {}];
+  let index = startIndex;
+  let map: TestNames = {};
 
   console.log(`1: all tests`);
   for (let testName in benchmarkDatasets) {
     console.log(`${index}: ${testName}`);
-    // @ts-ignore
     map[index++] = testName;
   }
 
@@ -28,20 +32,13 @@ import { question } from 'readline-sync';
   // run all test
   if (testIndex === 1) {
     for (let i = 2; i < index; i++) {
-      // @ts-ignore
       await runBenchmark(benchmarkDatasets[map[i]], result, jsonPath, map[i]);
     }
     return;
   }
 
   // run specific test
-  await runBenchmark(
-    // @ts-ignore
-    benchmarkDatasets[map[testIndex]],
-    null,
-    null,
-    // @ts-ignore
-    map[testIndex],
-  );
+  const testName = map[testIndex];
+  await runBenchmark(benchmarkDatasets[testName], null, null, testName);
   return;
 })();
