981b6ef6ad305259e9a4bb3bbb3737b9674f5ea0
chenshuming <csmdev@hotmail.com>
2021-12-23 15:19:43 +0800

    fix: update description

--- packages/benchmark_browser/src/layouts/profile.vue
+++ packages/benchmark_browser/src/layouts/profile.vue
@@ -1,7 +1,7 @@
 <template>
   <router-view />
   <h3>This is a JavaScript vs WebAssembly(WASM) benchmark.</h3>
-  <h3>The WASM files are written in C/C++ and compiled by emscripten.</h3>
+  <h3>The WASM files are written in C/C++ or Rust.</h3>
   <div class="logo-box-flex">
     <img
       alt="Origin.js logo"
