52bc48ccef21e48e1f6b160c0dd8ca441c4dfbe7
chenshuming <csmdev@hotmail.com>
2022-01-21 09:43:29 +0800

    add docs for github page

---
+++ docs/assets/_.5a7a00be.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.d12fa756.js";import{o,c as t}from"./vendor.36576d8b.js";const s={name:"404"},n={class:"text-p"};function r(a,c,p,_,d,f){return o(),t("p",n,"page not found...")}var l=e(s,[["render",r]]);export{l as default};
